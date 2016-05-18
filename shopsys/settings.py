9f9fee73aa35e9adf4ade13098a95cae601289be
Chouzhou <z846204062@hotmail.com>
2016-05-18 10:46:48 +0800

    models创建进入数据库

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(SETTINGS_DIR,''),
+            os.path.join(SETTINGS_DIR,'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
